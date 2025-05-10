5dee50c028ec5e56ca35d8ae25bb437d43143e95
Evilsaint1022_ <116245992+Evilsaint1022@users.noreply.github.com>
2025-05-10 17:49:29 +1200

    🌿Fixed Auto_kick.js Event🌿
    - The autokick.js event had problems finding the verifiedroleId that was set in settings. fixed now.

--- src/Events/Client/auto_kick.js
+++ src/Events/Client/auto_kick.js
@@ -3,40 +3,40 @@ const { Events } = require('discord.js');
 const db = require('../../Handlers/database');
 
 module.exports = {
-    name: Events.GuildMemberAdd,
-    async execute(member) {
-        const checkTime = 10 * 60 * 1000; // 10 minutes in milliseconds
+  name: Events.GuildMemberAdd,
+  async execute(member) {
+    const checkTime = 10 * 60 * 1000; // 10 minutes in milliseconds
 
-        // Ignore bots
-        if (member.user.bot) return;
+    // Ignore bots
+    if (member.user.bot) return;
 
-        const guildId = member.guild.id;
-        const guildName = member.guild.name;
+    const guildId = member.guild.id;
+    const guildName = member.guild.name;
 
-        // Retrieve the VerifiedRoleId from the database
-        let verifiedRoleId;
-        try {
-            const guildConfig = await db.settings.get(`${guildName}_${guildId}_verifiedRoleId`);
-            if (guildConfig && guildConfig.VerifiedRoleId) {
-                verifiedRoleId = guildConfig.VerifiedRoleId;
-            }
-        } catch (err) {
-            console.error(`Failed to retrieve role settings for guild ${guildName}_${guildId}:`, err);
-            return;
+    // Construct the settings key
+    const settingsKey = `${guildName}_${guildId}_verifiedRoleId`;
+
+    // Retrieve the VerifiedRoleId from the database
+    let verifiedRoleId;
+    try {
+      verifiedRoleId = await db.settings.get(settingsKey);
+    } catch (err) {
+      console.error(`Failed to retrieve role settings for guild ${settingsKey}:`, err);
+      return;
+    }
+
+    // Exit early if VerifiedRoleId isn't set
+    if (!verifiedRoleId) return;
+
+    // Set a timeout to check the member's role after 10 minutes
+    setTimeout(async () => {
+      try {
+        if (!member.roles.cache.has(verifiedRoleId)) {
+          await member.kick("Didn't Verify Quick Enough!");
         }
-
-        // Exit early if VerifiedRoleId isn't set
-        if (!verifiedRoleId) return;
-
-        // Set a timeout to check the member's role after 10 minutes
-        setTimeout(async () => {
-            try {
-                if (!member.roles.cache.has(verifiedRoleId)) {
-                    await member.kick('Didn\'t Verify Quick Enough!');
-                }
-            } catch (error) {
-                console.error(`Failed to kick ${member.user.tag}:`, error);
-            }
-        }, checkTime);
-    },
+      } catch (error) {
+        console.error(`Failed to kick ${member.user.tag}:`, error);
+      }
+    }, checkTime);
+  },
 };
