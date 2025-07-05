bf4fb039d1a729447406a2f26d0626c2fc349c32
Linix Red <linus.ordowski@outlook.com>
2025-07-05 10:30:08 +0200

    🌿 Added new RSS Topic to library 🌿

--- src/rssconfig.js
+++ src/rssconfig.js
@@ -5,7 +5,10 @@ const topics = [
     { name: 'National', url: 'https://www.rnz.co.nz/rss/national.xml' },
     { name: 'Environment', url: 'https://www.rnz.co.nz/rss/environment.xml' },
     { name: 'Business', url: 'https://www.rnz.co.nz/rss/business.xml' },
-    { name: 'Sport', url: 'https://www.rnz.co.nz/rss/sport.xml' }
+    { name: 'Sport', url: 'https://www.rnz.co.nz/rss/sport.xml' },
+    { name: "Mediatech", url: "https://www.rnz.co.nz/rss/media-technology.xml" }
+    // Add more, NAME CAN NOT CONTAIN *MULTIPLE UPPERCASE LETTERS* OR *SPACES* OR *SPECIAL CHARACTERS*
+    // Ill maybe add that in the future
 ];
 
 
