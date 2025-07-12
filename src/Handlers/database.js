db0fc77e8681e0379ab7aebfe4cc9b6d22eb7d1d
Evilsaint1022_ <116245992+Evilsaint1022@users.noreply.github.com>
2025-07-13 03:36:29 +1200

    🌿Clean-Up-Time!🌿
    Cleaned up many DB's handled by the database.js
    
    - Package Update
    - Updated gitignore

--- src/Handlers/database.js
+++ src/Handlers/database.js
@@ -3,15 +3,15 @@ const DotDB = require("dotdatabase").default;
 const db = {
 
     // Owners ID's DataBase -------------------------------------------------------------------------
-    owners: new DotDB("./src/Utilities/Owners/owners.json"),
+    owners: new DotDB("./src/Utilities/Storage/Settings/Owners/owners.json"),
     
     // -----------------------------------------------------------------------------------------
     vc: new DotDB("./src/Utilities/Storage/Settings/VoiceChannels/vc.json"),
     whitelisted: new DotDB("./src/Utilities/Storage/Settings/Whitelisted/whitelisted.json"),
-    balance: new DotDB("./src/Utilities/Storage/Global/Economy/Balance/balance.json"),
-    bank: new DotDB("./src/Utilities/Storage/Global/Economy/Bank/bank.json"),
-    levels: new DotDB("./src/Utilities/Storage/Levels/levels.json"),
-    levelroles: new DotDB("./src/Utilities/Storage/Levels/levelroles.json"),
+    balance: new DotDB("./src/Utilities/Storage/Economy/Balance/balance.json"),
+    bank: new DotDB("./src/Utilities/Storage/Economy/Bank/bank.json"),
+    levels: new DotDB("./src/Utilities/Storage/Economy/Levels/levels.json"),
+    levelroles: new DotDB("./src/Utilities/Storage/Settings/LevelRoles/levelroles.json"),
     settings: new DotDB(`./src/Utilities/Storage/Settings/settings.json`),
     starboard: new DotDB("./src/Utilities/Storage/Settings/Starboard/starboardsettings.json"),
     starboardids: new DotDB("./src/Utilities/Storage/Settings/Starboard/Starboard_Ids.json"),
@@ -21,9 +21,9 @@ const db = {
     lastbump: new DotDB("./src/Utilities/Storage/Settings/Bump/lastbump.json"),
     ai_history: new DotDB("./src/Utilities/Storage/Settings/AI/ai_history.json"),
     ai_nsfw_training: new DotDB("./src/Utilities/Storage/Settings/AI/ai_nsfw_training.json"),
-    shop: new DotDB("./src/Utilities/Storage/Shop/shop.json"),
-    inventory: new DotDB("./src/Utilities/Storage/Inventory/inventory.json"),
-    rss: new DotDB("./src/Utilities/Storage/Rss/rss.json")
+    shop: new DotDB("./src/Utilities/Storage/Economy/Shop/shop.json"),
+    inventory: new DotDB("./src/Utilities/Storage/Economy/Inventory/inventory.json"),
+    rss: new DotDB("./src/Utilities/Storage/Settings/Rss/rss.json")
     
 }
 
