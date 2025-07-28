e47bd2343bde6e23a3f0a9841ba7619a8d0d8a70
Evilsaint1022_ <116245992+Evilsaint1022@users.noreply.github.com>
2025-07-28 17:29:37 +1200

    🌿Slots Update🌿
    - Added Slots Application Command
    - Edited Blackjack console commands
    
    - Registered the slots application command in register-commands.js

--- src/Commands/Everyone/blackjack.js
+++ src/Commands/Everyone/blackjack.js
@@ -17,8 +17,8 @@ module.exports = {
         const ferns = '<:Ferns:1395219665638391818>';
 
         // console logs place here on purpose.
-        console.log(`[${new Date().toLocaleTimeString()}] ${guild.name} ${guild.id} ${interaction.user.username} used the BlackJack command.`);
-        console.log(`[${new Date().toLocaleTimeString()}] ${user.username} placed a bet of ${bet.toLocaleString()} Ferns.`);
+        console.log(`[♦️] [${new Date().toLocaleTimeString()}] ${guild.name} ${guild.id} ${interaction.user.username} used the BlackJack command.`);
+        console.log(`[♦️] [${new Date().toLocaleTimeString()}] ${user.username} placed a bet of ${bet.toLocaleString()} Ferns.`);
 
         let balance = await db.balance.get(balanceKey);
 
@@ -88,11 +88,11 @@ module.exports = {
             if (result) {
             if (result === 'win') {
                 balance += bet;
-            console.log(`[${new Date().toLocaleTimeString()}] ${user.username} Won a Bet of ${bet.toLocaleString()} Ferns.`);
+            console.log(`[♦️] [${new Date().toLocaleTimeString()}] ${user.username} Won a Bet of ${bet.toLocaleString()} Ferns.`);
              }
             else if (result === 'lose') {
                 balance -= bet;
-            console.log(`[${new Date().toLocaleTimeString()}] ${user.username} Lost a Bet of ${bet.toLocaleString()} Ferns.`);
+            console.log(`[♦️] [${new Date().toLocaleTimeString()}] ${user.username} Lost a Bet of ${bet.toLocaleString()} Ferns.`);
             }
 
                 await db.balance.set(balanceKey, balance);
