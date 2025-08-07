f29fe6b4652a1130de693283678e5c1a6c95427f
Evilsaint1022_ <116245992+Evilsaint1022@users.noreply.github.com>
2025-08-07 16:41:37 +1200

    🌿Adding Github Scrapper🌿
    - Added the Github scrapper back into the main discord bot for better Github repo updates to the support server.

---
+++ src/Functions/Github/scrapeCommits.js
@@ -0,0 +1,25 @@
+const checkAllCommits = require('./checkallcommits');
+const sendCommitNotification = require('./sendcommitnotification');
+
+async function scrapeCommits(client) {
+
+  while (true) {
+    try {
+      const newCommits = await checkAllCommits();
+
+      for (const commit of newCommits.reverse()) {
+        await sendCommitNotification(client, commit);
+      }
+
+      if (newCommits.length === 0) {
+      }
+
+    } catch (err) {
+      console.error('[GITHUB] Error during commit check:', err);
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 5000));
+  }
+}
+
+module.exports = scrapeCommits;
