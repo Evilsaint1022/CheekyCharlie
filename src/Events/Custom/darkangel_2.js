2b3e10aab39190791fbadc67f0ae5805405c057f
NZ-Linix <nz-linix@outlook.com>
2025-12-03 11:14:13 +0100

    🌿 Custom Auto Response update for Angel 🌿

--- src/Events/Custom/darkangel_2.js
+++ src/Events/Custom/darkangel_2.js
@@ -12,7 +12,7 @@ module.exports = {
         if ( matches.includes(content) ) {
 
             console.log(`[👩🏻] [DARK ANGEL] [${new Date().toLocaleDateString('en-GB')}] [${new Date().toLocaleTimeString("en-NZ", { timeZone: "Pacific/Auckland" })}] ${message.guild.name} ${message.guild.id} - Eclipse found in ${message.channel.name} ${message.channel.id}`);
-            await message.reply({ content: "Eclipse smelly rogue" })
+            await message.reply({ content: "Eclipse the best!" })
 
         }
     }
