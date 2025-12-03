5c3df289c1b1a9c5f7cbd9fb5883feb787612c03
NZ-Linix <nz-linix@outlook.com>
2025-12-03 10:48:38 +0100

    🌿 Custom Autoresponse for Angel 🌿

---
+++ src/Events/Custom/darkangel_2.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'messageCreate',
+
+    async execute(message) {
+        // Ignore bot messages
+        if (message.author.bot) return;
+
+        const content = message.content.toLowerCase();
+
+        const matches = ["eclipse", "Eclipse"]
+
+        if ( matches.includes(content) ) {
+
+            console.log(`[👩🏻] [DARK ANGEL] [${new Date().toLocaleDateString('en-GB')}] [${new Date().toLocaleTimeString("en-NZ", { timeZone: "Pacific/Auckland" })}] ${message.guild.name} ${message.guild.id} - Eclipse found in ${message.channel.name} ${message.channel.id}`);
+            await message.reply({ content: "Eclipse smelly rogue" })
+
+        }
+    }
+};
