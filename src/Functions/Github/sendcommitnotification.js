4e083ac560577b121f615cc7cafc6ddeaf6faecf
Evilsaint1022 <116245992+Evilsaint1022@users.noreply.github.com>
2026-02-06 14:02:28 +1300

    🌿 Github Function / Small Fix 🌿
    - Updated the `sendcommitnotification` embed message to be gold instead of red.

--- src/Functions/Github/sendcommitnotification.js
+++ src/Functions/Github/sendcommitnotification.js
@@ -77,7 +77,7 @@ const repoImage = await loadImage(repoImagePath);
     const embed = new EmbedBuilder()
       .setDescription(`# 🌿**__${repo} Repo Updates__**🌿\nㅤ\n${commitlink}\nㅤ\n${middle}\n${centeredmessage}\nㅤ\n${centeredauthor}\n${bottom}`)
       .setImage(repoImageUrl)
-      .setColor(0xDE4949)
+      .setColor(0xFFD700)
       .setTimestamp(new Date());
 
     await channel.send({ embeds: [embed] });
