dde35b31513410bae91ba32840167ecc628cc1f7
Evilsaint1022 <116245992+Evilsaint1022@users.noreply.github.com>
2026-02-09 17:08:48 +1300

    🌿 Updated sync_guilds.js 🌿
    - Updated the `sync_guilds.js` console logs so it looks tidy and matches the rest of cheekycharlies console logs.

--- src/Events/System/sync_guilds.js
+++ src/Events/System/sync_guilds.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'guildUpdate',
     async execute(oldGuild, newGuild, client) {
         if (oldGuild.name !== newGuild.name) {
-            console.log(`[GuildUpdate] ${oldGuild.name} -> ${newGuild.name}`);
+            console.log(`[⚙️] [GUILD UPDATE] [${new Date().toLocaleDateString('en-GB')}] [${new Date().toLocaleTimeString("en-NZ", { timeZone: "Pacific/Auckland" })}] ${oldGuild.name} -> ${newGuild.name}`);
         }
     }
 };
