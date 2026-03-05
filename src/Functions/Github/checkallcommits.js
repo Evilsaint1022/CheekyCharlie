ac5b6dbab126a8d36f9740b71943efb7636db1c3
Evilsaint1022 <116245992+Evilsaint1022@users.noreply.github.com>
2026-03-06 11:49:20 +1300

    Revert "🌿 Github Function Update 🌿"
    This reverts commit 1f99e836b1c9d094b166bf9893f7667f0eb523ee.

--- src/Functions/Github/checkallcommits.js
+++ src/Functions/Github/checkallcommits.js
@@ -35,9 +35,9 @@ async function checkAllCommits() {
 
     return newCommits;
   } catch (err) {
-    if ( err.message === "fetch is not a function" ) {
-        return[];
-    }
+    if ( err.message === "fetch is not a function" ) return [];
+    console.error(`[⭐] [Github] [${new Date().toLocaleDateString('en-GB')}] [${new Date().toLocaleTimeString("en-NZ", { timeZone: "Pacific/Auckland" })}] ${err.message}`);
+    return[];
   }
 }
 
