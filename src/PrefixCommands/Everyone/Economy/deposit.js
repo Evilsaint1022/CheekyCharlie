a1500879f381b1cc7a00d9d18cf0b20c5e18fff3
Evilsaint1022 <116245992+Evilsaint1022@users.noreply.github.com>
2026-04-01 21:43:37 +1300

    🌿 Transactions Update 🌿
    - Updated the Log transactions section making sure it gets the `banktransactions` channel from the settings database.
    
    - Updated the formatting of the embed message making it match the `bank-interest` messages.
    
    These transactions messages will be sent to all servers that have a `banktransactions` channel set.

--- src/PrefixCommands/Everyone/Economy/deposit.js
+++ src/PrefixCommands/Everyone/Economy/deposit.js
@@ -106,21 +106,21 @@ module.exports = {
       // ------------------------------------------------------
       // 4️⃣ Log transaction
       // ------------------------------------------------------
-       const channelId = '1481927633678762084';
 
       for (const guild of message.client.guilds.cache.values()) {
 
-          let channel = guild.channels.cache.get(channelId);
+        const guildKey = `${guild.id}`;
+        const settings = await db.settings.get(guildKey);
 
-          if (!channel) {
-              channel = await guild.channels.fetch(channelId).catch(() => null);
-          }
-
-          if (!channel) continue;
+        let channel = guild.channels.cache.get(settings.banktransactions);
+        if (!channel) {
+            channel = await guild.channels.fetch(settings.banktransactions).catch(() => null);
+            continue;
+        }
 
       const embedlog = new EmbedBuilder()
           .setTitle('💰・**__Transaction Logs__**')
-          .setDescription(`${bar}\n**__Username:__** \`${author.username}\`\n**__UserID:__** \`${author.id}\`\n\n**__Bank Deposit:__** ${ferns}\`${depositAmount.toLocaleString()}\`\n${bar}\n🌿 Thanks for using Bank-NZ!`)
+          .setDescription(`${bar}\n- **__Username:__** \`${author.username}\`\n- **__UserID:__** \`${author.id}\`\n\n- **__Bank Deposit:__** ${ferns}\`${depositAmount.toLocaleString()}\`\n${bar}\n🌿 Thanks for using Bank-NZ!`)
           .setColor(0x207e37)
           .setTimestamp()
           .setThumbnail(guild.iconURL())
