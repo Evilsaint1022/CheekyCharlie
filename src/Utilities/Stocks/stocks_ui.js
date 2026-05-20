c6f8b9926752aaacffdecfbed96474ad222413b2
NZ-Linix <nz-linix@outlook.com>
2026-05-20 16:54:58 +0200

    🌿 Upload Emojis + Emoji manager 🌿

---
+++ src/Utilities/Stocks/stocks_ui.js
@@ -0,0 +1,14 @@
+
+const emojis = {
+    "ferncoin": "<:FernCoin:1506670591556583464>",
+    stocks: {
+        "up": "<:stocks_up:1506670601367064727>",
+        "down": "<:stocks_down:1506670593817182218>",
+        "plus": "<:stocks_plus:1506670599961841784>",
+        "minus": "<:stocks_minus:1506670596526837932>",
+        "plus_percent": "<:stocks_plus_percent:1506670598535778364>",
+        "minus_percent": "<:stocks_minus_percent:1506670595218210948>"
+    }
+}
+
+module.exports = emojis;
