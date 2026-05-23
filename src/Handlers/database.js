3d81e010afccfeddebdb43380c8d87a88e57ea91
Evilsaint1022 <116245992+Evilsaint1022@users.noreply.github.com>
2026-05-23 15:40:54 +1200

    🌿 Economy Jobs Tax 🌿
    We are adding a Ecomomy Jobs Tax.
    
    - Updated the `jobslist.json` adding a tax to each one depending on the job and how much they earn.
    
    - Added a new `tax` DB to the `database.js` file.
    
    - Added the `tax` DB directory to the `gitignore` file.

--- src/Handlers/database.js
+++ src/Handlers/database.js
@@ -58,7 +58,8 @@ const db = {
     stolen: new DotDB("./src/Utilities/Storage/Economy/Stolen/stolen.json"),
     lives: new DotDB("./src/Utilities/Storage/Settings/Lives/lives.json"),
     joblist: new DotDB("./src/Utilities/Storage/JobList/joblist.json"),
-    workers: new DotDB("./src/Utilities/Storage/Economy/Jobs/workers.json")
+    workers: new DotDB("./src/Utilities/Storage/Economy/Jobs/workers.json"),
+    tax: new DotDB("./src/Utilities/Storage/Economy/Tax/tax.json")
 }
 
 module.exports = db;
