67d692f70940e7e3da01804a65643f4ff1e5c2d0
Evilsaint1022 <116245992+Evilsaint1022@users.noreply.github.com>
2026-09-05 20:11:50 +1200

    🌿 Updated Reaction Events 🌿
    - Added a return if there is no `message.guild`.
    
    Trying to prevent the reaction events from going off in the dms and causing a error.
    
    - Added a `testing` channel_Id to the `threadCloseNotifier.js` & `dmListener.js` Events.
    
    This is for the modmail.

--- src/Events/Custom/shrimp.js
+++ src/Events/Custom/shrimp.js
@@ -4,7 +4,7 @@ module.exports = {
     name: Events.MessageCreate,
     once: false, // Set to false to allow multiple messages to trigger this event
     async execute(message) {
-
+        if (!message.guild) return;
         if (message.author.bot) return;
         if (message.webhookId) return;
         if (message.content.includes(':')) return;
